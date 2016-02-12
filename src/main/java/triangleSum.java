4bb960428f1e274c309379fef0d0abe71936d586
sevgi <sevgickmk.7@gmail.com>
2016-02-12 20:31:53 +0200

    Update

--- src/main/java/triangleSum.java
+++ src/main/java/triangleSum.java
@@ -17,15 +17,14 @@ public class triangleSum {
             for (int c = 0; c < data[r].length - 1; c++) {
                 int choose = data[r][c];
                 data[r - 1][c] += isNotPrime(Math.max(data[r][c], data[r][c + 1]));
-               // System.out.println("notPrime"+isNotPrime(Math.max(data[r][c], data[r][c + 1])));
             }
         }
        System.out.println("sum="+data[0][0]);
 
     }
-
+  //for (int=1;..) for this questions valid
     public static int isNotPrime(int num) {
-        for (int i = 2; i < num; i++) {
+        for (int i = 1; i < num; i++) {
             if (num % i == 0)
                 return num;
         }
